26f12614016546b6f3e4b2094a7039d2b2d318e6
Lorenz Meier <lm@inf.ethz.ch>
2014-12-08 15:12:03 +0100

    Add unit test to mode filter

---
+++ unittests/tests.cpp
@@ -0,0 +1,26 @@
+/**
+ * UNIT TESTS for flow code
+ */
+
+
+#include <stdio.h>
+#include "../src/sonar_mode_filter.h"
+
+int main(int argc, char *argv[]) {
+
+	for (unsigned i = 0; i < 25; i++) {
+
+		unsigned in = i;
+
+		if (in % 5 == 0) {
+			in*= 10;
+		}
+
+		float inf = in / 10.0f;
+
+		float out = insert_sonar_value_and_get_mode_value(inf);
+
+		printf("in: %f\tout: %f\n", inf, out);
+	}
+	
+}
