20b2474bbc4f58cea540f06f25ff2362893c60c8
James Goppert <james.goppert@gmail.com>
2016-11-09 13:49:07 -0500

    Allow gcc 5.4.1

--- makefiles/baremetal/toolchain_gnu-arm-eabi.mk
+++ makefiles/baremetal/toolchain_gnu-arm-eabi.mk
@@ -50,7 +50,7 @@ OBJDUMP			 = $(CROSSDEV)objdump
 
 # Check if the right version of the toolchain is available
 #
-CROSSDEV_VER_SUPPORTED	 = 4.7.4 4.7.5 4.7.6 4.8.4 4.9.3
+CROSSDEV_VER_SUPPORTED	 = 4.7.4 4.7.5 4.7.6 4.8.4 4.9.3 5.4.1
 CROSSDEV_VER_FOUND	 = $(shell $(CC) -dumpversion)
 
 ifeq (,$(findstring $(CROSSDEV_VER_FOUND), $(CROSSDEV_VER_SUPPORTED)))
@@ -277,3 +277,4 @@ define SYM_TO_BIN
 	@$(MKDIR) -p $(dir $2)
 	$(Q) $(OBJCOPY) -O binary $1 $2
 endef
+
