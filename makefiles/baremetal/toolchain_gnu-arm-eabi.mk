f189d77f08578bd41cf897f222809f983a66b43e
Julien Lecoeur <julien.lecoeur@gmail.com>
2017-09-15 14:22:45 +0200

    Allow GCC 7.2.0

--- makefiles/baremetal/toolchain_gnu-arm-eabi.mk
+++ makefiles/baremetal/toolchain_gnu-arm-eabi.mk
@@ -50,7 +50,7 @@ OBJDUMP			 = $(CROSSDEV)objdump
 
 # Check if the right version of the toolchain is available
 #
-CROSSDEV_VER_SUPPORTED	 = 4.7.4 4.7.5 4.7.6 4.8.2 4.8.4 4.9.3 5.4.1 7.1.0
+CROSSDEV_VER_SUPPORTED	 = 4.7.4 4.7.5 4.7.6 4.8.2 4.8.4 4.9.3 5.4.1 7.1.0 7.2.0
 CROSSDEV_VER_FOUND	 = $(shell $(CC) -dumpversion)
 
 ifeq (,$(findstring $(CROSSDEV_VER_FOUND), $(CROSSDEV_VER_SUPPORTED)))
