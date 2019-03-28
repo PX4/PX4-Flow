a55b66d04e7416c408a0c70a4a7e798cbace8320
Sergei Zobov <sergey@szobov.ru>
2019-03-28 14:57:59 +0300

    src/modules/flow/settings.c: enable gyro compensation by default
    It was suspiciously changed in commit, that doesn't related to gyro compensation
    at all, here:
    
    https://github.com/PX4/Flow/commit/6d584340e5691e81146fbd3fce17881380fec8ba#diff-cfb42647f460cb87f3decb6b388a5cb1R179
    
    So, after fix from commit 7aa35163514cf908f5cb90bfb56cf69623906524 it makes
    estimation much better.

--- src/modules/flow/settings.c
+++ src/modules/flow/settings.c
@@ -182,8 +182,8 @@ void global_data_reset_param_defaults(void){
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_HIST_FILTER], "BFLOW_HIST_FIL");
 	global_data.param_access[PARAM_BOTTOM_FLOW_HIST_FILTER] = READ_WRITE;
 
-//	global_data.param[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = 0;
-	global_data.param[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = 0;
+	/* global_data.param[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = 0; */
+	global_data.param[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = 1;
 	strcpy(global_data.param_name[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION], "BFLOW_GYRO_COM");
 	global_data.param_access[PARAM_BOTTOM_FLOW_GYRO_COMPENSATION] = READ_WRITE;
 
